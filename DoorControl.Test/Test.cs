0508e7a7ffe2e9f6695a394c8d6590455f2af034
Thomas <thomas.moeller.1996@gmail.com>
2019-02-14 11:51:11 +0100

    Tilføjelse af flere test

--- DoorControl.Test/Test.cs
+++ DoorControl.Test/Test.cs
@@ -23,17 +23,84 @@ namespace Door.Control.Test
         {
           alarm=new FakeAlarm();
           door=new FakeDoor();
-          entryNotification=new EntryNotfication();
-          userValidation=new UserValidation();
+          entryNotification=new FakeEntryNotfication();
+          userValidation=new FakeUserValidation();
           _uut=new Door_control.DoorControl(door, userValidation, entryNotification, alarm);
         }
 
         [Test]
-        public void RequestEntry_RequestEntryCalled_OpenCalled()
+        public void RequestEntry_RequestEntryCalled_IdEqualsTrue_ValidateEntryRequestIsCalled()
+        {
+            _uut.RequestEntry(1);
+            FakeUserValidation userValidationCheck = (FakeUserValidation)userValidation;
+            Assert.That(userValidationCheck.ValidateEntryRequestNo, Is.EqualTo(1));
+        }
+        [Test]
+        public void RequestEntry_RequestEntryCalled_IdEqualsTrue_OpenCalled()
         {
             _uut.RequestEntry(1);
             FakeDoor doorCheck = (FakeDoor) door;
             Assert.That(doorCheck.OpenNo, Is.EqualTo(1));
         }
+
+        [Test]
+        public void RequestEntry_RequestEntryCalled_IdEqualsTrue_NotifyEntryGrantedIsCalled()
+        {
+            _uut.RequestEntry(1);
+            FakeEntryNotfication entryNotficationCheck = (FakeEntryNotfication)entryNotification;
+            Assert.That(entryNotficationCheck.NotifyEntryGrantedNo, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RequestEntry_RequestEntryCalled_IdEqualsTrue_NotifyEntryDeniedIsNotCalled()
+        {
+            _uut.RequestEntry(1);
+            FakeEntryNotfication entryNotficationCheck = (FakeEntryNotfication)entryNotification;
+            Assert.That(entryNotficationCheck.NotifyEntryDeniedNo, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void RequestEntry_RequestEntryCalled_IdEqualsFalse_NotifyEntryDeniedIsCalled()
+        {
+            _uut.RequestEntry(2);
+            FakeEntryNotfication entryNotficationCheck = (FakeEntryNotfication)entryNotification;
+            Assert.That(entryNotficationCheck.NotifyEntryDeniedNo, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RequestEntry_RequestEntryCalled_IdEqualsFalse_NotifyEntryGrantedIsNotCalled()
+        {
+            _uut.RequestEntry(2);
+            FakeEntryNotfication entryNotficationCheck = (FakeEntryNotfication)entryNotification;
+            Assert.That(entryNotficationCheck.NotifyEntryGrantedNo, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void RequestEntry_RequestEntryCalled_IdEqualsTrue_ClosedCalled()
+        {
+            _uut.RequestEntry(1);
+            FakeDoor doorCheck = (FakeDoor)door;
+            Assert.That(doorCheck.CloseNo, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void DoorOpen_DoorOpenIsCalled_StateEqualsDoorClosed_ClosedCalled()
+        {
+            _uut.DoorOpen();
+            FakeDoor doorCheck = (FakeDoor)door;
+            Assert.That(doorCheck.CloseNo, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void DoorOpen_DoorOpenIsCalled_StateEqualsDoorClosed_RaiseAlarmCalled()
+        {
+            _uut.DoorOpen();
+            FakeAlarm alarmCheck = (FakeAlarm) alarm;
+            Assert.That(alarmCheck.Triggers, Is.EqualTo(1));
+        }
+
+
+
+
     }
 }
